d2c2df81344e2d901f47d97a559521fb6bbb8a2e
skipmcne <skipmcne@gmail.com>
2018-06-20 08:04:07 -0400

    CEM wup

--- project.clj
+++ project.clj
@@ -4,3 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
+
+  ;;; Profiles
+  ;; Each active profile gets merged into the project map. The :dev
+  ;; and :user profiles are active by default, but the latter should be
+  ;; looked up in ~/.lein/profiles.clj rather than set in project.clj.
+  ;; Use the with-profiles higher-order task to run a task with a
+  ;; different set of active profiles.
+  ;; See `lein help profiles` for a detailed explanation.
+
--- src/skip/dmtools.clj
+++ src/skip/dmtools.clj
@@ -1,6 +1,16 @@
 (ns skip.dmtools)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns user
+  "Defines helper functions for starting and stopping the application from the REPL."
+  (:require
+   [clojure.tools.namespace.repl :as repl]))
+
+(defn start
+  ;; In a real application this would actually start something.
+  []
+  (println "Start completed"))
+
+(defn reset []
+  (repl/refresh :after 'user/start))
+
+(println "Custom user.clj loaded.")
